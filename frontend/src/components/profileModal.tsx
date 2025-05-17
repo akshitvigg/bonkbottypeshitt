9a7f78ef79e2698d2abcd7651f6cee4fb7c49126
Akshit Vig <akshitvig48@gmail.com>
2025-05-17 23:53:17 +0530

    button styled

--- frontend/src/components/profileModal.tsx
+++ frontend/src/components/profileModal.tsx
@@ -26,7 +26,7 @@ const ProfileModal = ({ open, image, username }: any) => {
             <div>
               <button
                 onClick={logout}
-                className=" bg-red-100 p-3 translate-y-8 -translate-x-24 rounded-2xl text-red-500 cursor-pointer"
+                className=" transition-all duration-250  bg-red-100 p-3 hover:bg-red-500 hover:text-red-100 translate-y-8 -translate-x-24 rounded-2xl text-red-500 cursor-pointer"
               >
                 {" "}
                 Logout
